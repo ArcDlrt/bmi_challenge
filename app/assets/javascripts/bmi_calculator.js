eb153e1bb5b7566b5577fe17e74f7eb44c8c1e52
ArcDlrt <arc.ofyours@gmail.com>
2017-12-01 22:40:22 -0700

    BMI controller, views and tests

--- app/assets/javascripts/bmi_calculator.js
+++ app/assets/javascripts/bmi_calculator.js
@@ -1,3 +1,14 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function(){
+  $('#bmi_unit_system').change(function(){
+    var weight = $('#weight_unit');
+    var height = $('#height_unit');
+
+    if($('#bmi_unit_system').val() == 'e' ){
+      weight.text('lb');
+      height.text('in');
+    } else {
+      weight.text('kg');
+      height.text('m');
+    }
+  });
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!
+
   def after_sign_in_path_for(resource)
-    bmi_calculator_path
+    bmi_calculators_path
   end
 end
